037ec4245d87d26921af22b7ba2e48f803c83b1a
GonzaloG146 <gg260877@gmail.com>
2026-09-10 02:04:58 +0000

    Actualizacion

--- BuscadorPokemon/src/app/services/pokemon-storag--skip-test.service.ts
+++ BuscadorPokemon/src/app/services/pokemon-storag--skip-test.service.ts
@@ -28,4 +28,39 @@ export class PokemonStoragSkipTestService {
       this.misPokemons.set(JSON.parse(data));
     }
    }
+   //-1. Obtener datos de la API
+   buscarEnAPI(nombreOId: string){
+    return this.http.get<any>('https://pokeapi.co/api/v2/pokemon/${nombreOId.toLoweCase}');
+
+   }
+
+   //-2. Guardar/Crear nuevo Pokemon dentro del el maleto.
+   
+   guardarPokemon(nuevo: PokemonTarjeta){
+      const actualizados= [...this.misPokemons(), nuevo];
+      this.misPokemons.set(actualizados);
+      localStorage.setItem(this.STORAGE_KEY, JSON.stringify(actualizados));
+
+     
+
+   }
+ //3. Actualizar Pokemon
+
+    actualizarFavoritoI(id: number){
+      const actualizados = this.misPokemons().map(poke => {
+        if(poke.id === id){
+          return {...poke, esFavorito: !poke.esFavorito}
+        }
+        return poke;
+      });
+      this.misPokemons.set(actualizados)
+};
+
+//4. Eliminar Pokemon del maleto.
+eliminarPokemon(id: number){
+  const filtrados = this.misPokemons().filter(poke => poke.id !== id);
+  this.misPokemons.set(filtrados);
+  localStorage.setItem(this.STORAGE_KEY, JSON.stringify(filtrados));
+}
+
 }
